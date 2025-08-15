4015a6e2798f3784fd1e5261fccb31a40e0e06ab
Petr Vraník <Petr.Vranik@mgm-tp.com>
2025-08-15 18:32:27 +0200

    jpa

--- src/main/java/net/suteren/stardict/wiktionary2stardict/jpa/repository/WordDefinitionRepository.java
+++ src/main/java/net/suteren/stardict/wiktionary2stardict/jpa/repository/WordDefinitionRepository.java
@@ -1,11 +1,21 @@
 package net.suteren.stardict.wiktionary2stardict.jpa.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import net.suteren.stardict.wiktionary2stardict.jpa.entity.WordDefinitionEntity;
+import net.suteren.stardict.wiktionary2stardict.jpa.entity.WordDefinitionPair;
 
 @Repository public interface WordDefinitionRepository extends JpaRepository<WordDefinitionEntity, UUID> {
+
+	@Query("select e1, e2 from WordDefinitionEntity e1, WordDefinitionEntity e2 where e1.language = :fromLang and e2.language == :toLang and exists (select s1 from e1.senses s1 where s1.word in (select s2.word from e2.senses s2))")
+	List<WordDefinitionPair> findAllTranslations(String fromLang, String toLAng);
+
+	@Query("select e1, e2 from WordDefinitionEntity e1, WordDefinitionEntity e2 where e1.language = :fromLang and e2.language == :toLang and e1.word = :word and exists (select s1 from e1.senses s1 where s1.word in (select s2.word from e2.senses s2))")
+	List<WordDefinitionPair> findTranslation(String fromLang, String toLAng, String word);
+
 }
